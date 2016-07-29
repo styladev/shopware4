c55c45376473313da4269a0588e233b272ab4d35
ssachtleben <ssachtleben@gmail.com>
2016-07-29 13:27:30 +0200

    allow request parameters in magazine urls

--- StylaSEO/Bootstrap.php
+++ StylaSEO/Bootstrap.php
@@ -90,11 +90,12 @@ class Shopware_Plugins_Frontend_StylaSEO_Bootstrap extends Shopware_Components_P
     public function onPreDispatch(Enlight_Controller_EventArgs $args){
 
         $request  = $args->getRequest();
+        $url = strtok($request->getRequestUri(),'?');
 
         $this->_styla_username = $this->Config()->get('styla_username');
         $this->_magazin_basedir = $this->Config()->get('styla_basedir', 'magazin');
 
-        if( ($request->getRequestUri() == '/'.$this->_magazin_basedir || strpos($request->getRequestUri(), '/'.$this->_magazin_basedir.'/') !== false) ) {
+        if ($url == '/'.$this->_magazin_basedir || strpos($url, '/'.$this->_magazin_basedir.'/') !== false){
 		    $controller	= 'magazin';
         } else if( ($request->getRequestUri() == '/stylaapi' || strpos($request->getRequestUri(), '/stylaapi/') !== false) ) {
 		    $controller	= 'stylaapi';
@@ -128,8 +129,9 @@ class Shopware_Plugins_Frontend_StylaSEO_Bootstrap extends Shopware_Components_P
     }
 
     public function onGetControllerPathFrontend(Enlight_Event_EventArgs $args){
-        $request  = $args->getRequest();
-        if ($request->getRequestUri() == '/'.$this->_magazin_basedir || strpos($request->getRequestUri(), '/'.$this->_magazin_basedir.'/') !== false){
+        $request = $args->getRequest();
+        $url = strtok($request->getRequestUri(),'?');
+        if ($url == '/'.$this->_magazin_basedir || strpos($url, '/'.$this->_magazin_basedir.'/') !== false){
             return $this->Path() . 'Controllers/Frontend/Magazin.php';
         }
 	    else{
